f64fffa612ffa617a4d05914bdec98b48e300dc6
ahzhong <trungtv@zigexn.vn>
2013-08-19 10:51:21 +0700

    Half way chapter 10 (done at school)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,11 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
+    redirect_to root_url if signed_in?
   	@user = User.new
   end
 
@@ -51,13 +53,6 @@ class UsersController < ApplicationController
   	end
 
     #Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
